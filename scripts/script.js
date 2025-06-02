aca9145120105953a99bb4ec4491be40b4c70ea7
Assin Anna <assinannameckal@gmail.com>
2025-06-02 20:19:23 +0530

    updated

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+ document.addEventListener("DOMContentLoaded", function() {
+    const faqTitles = document.querySelectorAll('.faq-title');
+
+    faqTitles.forEach((title) => {
+      title.addEventListener('click', function() {
+        this.classList.toggle('active');
+
+        const answer = this.nextElementSibling;
+        if (answer.style.display === "block") {
+          answer.style.display = "none";
+        } else {
+          answer.style.display = "block";
+        }
+      });
+    });
+  });
